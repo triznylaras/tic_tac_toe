7d1584bd687c004111af765d929b1e4a1ff03774
triznylaras <triznylaras@gmail.com>
2021-10-21 16:14:33 +0700

    Create Cell class & setup RSpec

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in tic_tac_toe.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tic_tac_toe/version"
+
+module TicTacToe
+  class Error < StandardError; end
+  # Your code goes here...
+end
