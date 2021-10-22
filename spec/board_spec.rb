1125e1f1a4790a4a63dbfb41aec420cc37f64b2e
triznylaras <triznylaras@gmail.com>
2021-10-22 16:50:33 +0700

    Add spec for grid

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,7 +7,24 @@ module TicTacToe
       it "initializes the board with a grid" do
         expect { Board.new(grid: "grid") }.to_not raise_error
       end
+
+      it "sets the grid with three rows by default" do
+        board = Board.new
+        expect(board.grid.size).to_eq(3)
+      end
+      
+      it "creates three things in each row by default" do
+        board = Board.new
+        board.grid.each do |row|
+          expect(row.size).to_eq(3)
+        end
+      end
     end
- 
+    context "#grid" do
+      it "returns the grid" do
+        board = Board.new(grid: "blah")
+        expect(board.grid).to eq "blah"
+      end
+    end
   end
 end
