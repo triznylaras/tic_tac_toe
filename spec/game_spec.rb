8ab01a39e29e0dde43278051baa5bbc01698bbd0
triznylaras <triznylaras@gmail.com>
2021-10-25 16:33:24 +0700

    Add switch_players spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -19,5 +19,21 @@ module TicTacToe
         expect(game.other_player).to eq bob
       end
     end
+
+    context "#switch_players" do
+      it "will set @current_player to @other_player" do
+        game = Game.new([bob, frank])
+        other_player = game.other_player
+        game.switch_players
+        expect(game.current_player).to eq other_player
+      end
+     
+      it "will set @other_player to @current_player" do
+        game = Game.new([bob, frank])
+        current_player = game.current_player
+        game.switch_players
+        expect(game.other_player).to eq current_player
+      end
+    end
   end
 end
