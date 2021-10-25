774853c79b4bd0e250ac44411eb0da9ada98832a
triznylaras <triznylaras@gmail.com>
2021-10-25 15:45:35 +0700

    Change deprecated any_instance method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,13 +8,13 @@ module TicTacToe
 
     context "#initialize" do
       it "randomly selects a current_player" do
-        Array.any_instance.stub(:shuffle) { [frank, bob] }
+        allow_any_instance_of(Array).to receive(:shuffle) { [frank, bob] }
         game = Game.new([bob, frank])
         expect(game.current_player).to eq frank
       end
  
       it "randomly selects an other player" do
-        Array.any_instance.stub(:shuffle) { [frank, bob] }
+        allow_any_instance_of(Array).to receive(:shuffle) { [frank, bob] }
         game = Game.new([bob, frank])
         expect(game.other_player).to eq bob
       end
