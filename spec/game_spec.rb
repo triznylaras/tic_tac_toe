1e39b73a157f1ac19349435588421d777c64b9cc
triznylaras <triznylaras@gmail.com>
2021-10-25 15:25:00 +0700

    Add tests for instance var in Game class

---
+++ spec/game_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+module TicTacToe
+  describe Game do
+
+    let (:bob) { Player.new({color: "X", name: "bob"}) }
+    let (:frank) { Player.new({color: "O", name: "frank"}) }
+
+    context "#initialize" do
+      it "randomly selects a current_player" do
+        Array.any_instance.stub(:shuffle) { [frank, bob] }
+        game = Game.new([bob, frank])
+        expect(game.current_player).to eq frank
+      end
+ 
+      it "randomly selects an other player" do
+        Array.any_instance.stub(:shuffle) { [frank, bob] }
+        game = Game.new([bob, frank])
+        expect(game.other_player).to eq bob
+      end
+    end
+  end
+end
