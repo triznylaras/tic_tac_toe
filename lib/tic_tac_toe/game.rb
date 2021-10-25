9fa9d6eeffc7a1a7c2c76fb875a09d1e5bdd6110
triznylaras <triznylaras@gmail.com>
2021-10-25 15:45:05 +0700

    Add switch players method

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -6,5 +6,9 @@ module TicTacToe
       @board = board
       @current_player, @other_player = players.shuffle
     end
+
+    def switch_players
+      @current_player, @other_player = @other_player, @current_player
+    end
   end
 end
