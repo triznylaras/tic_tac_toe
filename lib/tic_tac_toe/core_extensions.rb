563c7400b4d4510be6bd11f83b9ff50af9f3fde4
triznylaras <triznylaras@gmail.com>
2021-11-05 16:29:02 +0700

    refactor core extensions and game class

--- lib/tic_tac_toe/core_extensions.rb
+++ lib/tic_tac_toe/core_extensions.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
+# class for inspect an array
 class Array
   def all_empty?
-    self.all? { |element| element.to_s.empty? }
+    all? { |element| element.to_s.empty? }
   end
 
   def all_same?
-    self.all? { |element| element == self[0] }
+    all? { |element| element == self[0] }
   end
 
   def any_empty?
-    self.any? { |element| element.to_s.empty? }
+    any? { |element| element.to_s.empty? }
   end
 
   def none_empty?
--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 module TicTacToe
+  # class to set the game to  be played
   class Game
     attr_reader :players, :board, :current_player, :other_player
+
     def initialize(players, board = Board.new)
       @players = players
       @board = board
@@ -21,33 +25,34 @@ module TicTacToe
 
     def game_over_message
       return "#{current_player.name} won!" if board.game_over == :winner
-      return "The game ended in a tie" if board.game_over == :draw
+      return 'The game ended in a tie' if board.game_over == :draw
     end
 
-    def game_over
-      puts game_over_message
-      board.formatted_grid
+    def board_game_over(board)
+      if board.game_over
+        puts game_over_message
+        board.formatted_grid
+        true
+      else
+        switch_players
+        false
+      end
     end
 
     def play
       puts "#{current_player.name} has randomly been selected as the first player"
-      while true
+      loop do
         board.formatted_grid
-        puts ""
+        puts ''
         puts solicit_move
         x, y = get_move
         board.set_cell(x, y, current_player.color)
-        if board.game_over
-          game_over
-          return
-        else
-          switch_players
-        end
+        return if board_game_over(board) == true
       end
     end
-     
+
     private
-     
+
     def human_move_to_coordinate(human_move)
       mapping = {
         "1" => [0, 0],
