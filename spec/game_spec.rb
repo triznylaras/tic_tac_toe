0b09edcdec45d4a9701655b73deec605917ca785
triznylaras <triznylaras@gmail.com>
2021-11-09 12:37:28 +0700

    refactor spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,34 +1,33 @@
-require "spec_helper"
+require 'spec_helper'
 
 module TicTacToe
   describe Game do
+    let (:bob) { Player.new({ color: 'X', name: 'bob' }) }
+    let (:frank) { Player.new({ color: 'O', name: 'frank' }) }
 
-    let (:bob) { Player.new({color: "X", name: "bob"}) }
-    let (:frank) { Player.new({color: "O", name: "frank"}) }
-
-    context "#initialize" do
-      it "randomly selects a current_player" do
+    context '#initialize' do
+      it 'randomly selects a current_player' do
         allow_any_instance_of(Array).to receive(:shuffle) { [frank, bob] }
         game = Game.new([bob, frank])
         expect(game.current_player).to eq frank
       end
- 
-      it "randomly selects an other player" do
+
+      it 'randomly selects an other player' do
         allow_any_instance_of(Array).to receive(:shuffle) { [frank, bob] }
         game = Game.new([bob, frank])
         expect(game.other_player).to eq bob
       end
     end
 
-    context "#switch_players" do
-      it "will set @current_player to @other_player" do
+    context '#switch_players' do
+      it 'will set @current_player to @other_player' do
         game = Game.new([bob, frank])
         other_player = game.other_player
         game.switch_players
         expect(game.current_player).to eq other_player
       end
-     
-      it "will set @other_player to @current_player" do
+
+      it 'will set @other_player to @current_player' do
         game = Game.new([bob, frank])
         current_player = game.current_player
         game.switch_players
@@ -36,26 +35,25 @@ module TicTacToe
       end
     end
 
-    context "#solicit_move" do
-      it "asks the player to make a move" do
+    context '#solicit_move' do
+      it 'asks the player to make a move' do
         game = Game.new([bob, frank])
         allow(game).to receive(:current_player) { bob }
-        expected = "bob: Enter a number between 1 and 9 to make your move"
+        expected = 'bob: Enter a number between 1 and 9 to make your move'
         expect(game.solicit_move).to eq expected
       end
     end
 
-    context "#get_move" do
+    context '#get_move' do
       it "converts human_move of '1' to [0, 0]" do
         game = Game.new([bob, frank])
-        expect(game.get_move("1")).to eq [0, 0]
+        expect(game.get_move('1')).to eq [0, 0]
       end
-     
+
       it "converts human_move of '1' to [0, 0]" do
         game = Game.new([bob, frank])
-        expect(game.get_move("7")).to eq [0, 2]
+        expect(game.get_move('7')).to eq [0, 2]
       end
     end
-    
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,1 @@
-require_relative "../lib/tic_tac_toe.rb"
+require_relative '../lib/tic_tac_toe'
