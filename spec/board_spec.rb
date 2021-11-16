917f2246ae1873c837262a70249823b6da8a9d84
triznylaras <triznylaras@gmail.com>
2021-11-16 14:27:22 +0700

    refactor board and game spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,19 +3,20 @@ require 'spec_helper'
 module TicTacToe
   Cat = Struct.new(:value)
   TestCell = Struct.new(:value)
+
   describe Board do
+    let(:board) { Board.new }
+
     context '#initialize' do
       it 'initializes the board with a grid' do
         expect { Board.new(grid: 'grid') }.to_not raise_error
       end
 
       it 'sets the grid with three rows by default' do
-        board = Board.new
         expect(board.grid.size).to eq(3)
       end
 
       it 'creates three things in each row by default' do
-        board = Board.new
         board.grid.each do |row|
           expect(row.size).to eq(3)
         end
@@ -52,20 +53,17 @@ module TicTacToe
 
     context '#game_over' do
       it 'returns :winner if winner? is true' do
-        board = Board.new
         allow(board).to receive(:winner?) { true }
         expect(board.game_over).to eq :winner
       end
 
       it 'returns :draw if winner? is false and draw? is true' do
-        board = Board.new
         allow(board).to receive(:winner?) { false }
         allow(board).to receive(:draw?) { true }
         expect(board.game_over).to eq :draw
       end
 
       it 'returns false if winner? is false and draw? is false' do
-        board = Board.new
         allow(board).to receive(:winner?) { false }
         allow(board).to receive(:draw?) { false }
         expect(board.game_over).to be_falsey
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,31 +4,30 @@ module TicTacToe
   describe Game do
     let(:bob) { Player.new({ color: 'X', name: 'bob' }) }
     let(:frank) { Player.new({ color: 'O', name: 'frank' }) }
+    let(:game) { Game.new([bob, frank]) }
 
     context '#initialize' do
-      it 'randomly selects a current_player' do
+      before :each do
         allow_any_instance_of(Array).to receive(:shuffle) { [frank, bob] }
-        game = Game.new([bob, frank])
+      end
+
+      it 'randomly selects a current_player' do
         expect(game.current_player).to eq frank
       end
 
       it 'randomly selects an other player' do
-        allow_any_instance_of(Array).to receive(:shuffle) { [frank, bob] }
-        game = Game.new([bob, frank])
         expect(game.other_player).to eq bob
       end
     end
 
     context '#switch_players' do
       it 'will set @current_player to @other_player' do
-        game = Game.new([bob, frank])
         other_player = game.other_player
         game.switch_players
         expect(game.current_player).to eq other_player
       end
 
       it 'will set @other_player to @current_player' do
-        game = Game.new([bob, frank])
         current_player = game.current_player
         game.switch_players
         expect(game.other_player).to eq current_player
@@ -37,7 +36,6 @@ module TicTacToe
 
     context '#solicit_move' do
       it 'asks the player to make a move' do
-        game = Game.new([bob, frank])
         allow(game).to receive(:current_player) { bob }
         expected = 'bob: Enter a number between 1 and 9 to make your move'
         expect(game.solicit_move).to eq expected
@@ -46,12 +44,10 @@ module TicTacToe
 
     context '#get_move' do
       it "converts human_move of '1' to [0, 0]" do
-        game = Game.new([bob, frank])
         expect(game.get_move('1')).to eq [0, 0]
       end
 
       it "converts human_move of '1' to [0, 0]" do
-        game = Game.new([bob, frank])
         expect(game.get_move('7')).to eq [0, 2]
       end
     end
